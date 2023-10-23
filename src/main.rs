620639baf2413246062a8246a06d632fd00c27e8
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-10-23 16:51:26 -0400

    feat: calculate average

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut number_list_string = String::new();
+    println!("Introduce a number lists separates with colon:");
+
+    // Get the list of numbers
+    stdin()
+        .read_line(&mut number_list_string)
+        .expect("Did not enter a correct string");
+
+    if number_list_string.len() < 1 {
+        println!("You need enter numbers");
+    }
+
+    number_list_string = number_list_string.trim().to_string();
+
+    let number_list: Vec<usize> = number_list_string
+        .split(",")
+        .map(|item| item.parse().unwrap_or(0))
+        .collect();
+
+    let average = calculate_average(number_list);
+
+    println!("The number average is: {}", average)
+}
+
+fn calculate_average(numbers: Vec<usize>) -> usize {
+    let number_quantity = numbers.len();
+
+    let mut number_total: usize = 0;
+
+    for number in numbers {
+        number_total += number;
+    }
+
+    number_total / number_quantity
+}
+
+fn get_median(numbers: Vec<usize>) -> usize {
+    todo!()
+}
+
+fn calculate_standard_deviation(numbers: Vec<usize>) -> usize {
+    todo!()
+}
+
+fn calculate_range(numbers: Vec<usize>) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_media() {
+        let numbers: Vec<usize> = vec![1, 2, 3, 4, 5];
+        assert_eq!(calculate_average(numbers), 3);
+    }
 }
